5428cb49548de4eaaa30b34046b4c958959b88b3
Bruna <70817817+brunacaveion@users.noreply.github.com>
2022-07-14 14:20:57 -0300

    Add files via upload

---
+++ CriandoFuncoesEmCodigos.cpp
@@ -0,0 +1,15 @@
+// Função para exibir menu de escolha de ticket
+#include <iostream>
+#include "FuncoesMenu.h"
+// o include <> vai procurar no diretório padrão do compilador
+// include "" procura no diretório corrente, atual do meu programa
+
+
+int main()
+{
+
+	ExibirMenu();
+	system("PAUSE");
+	return 0;
+}
+
